599aca19f72161626707e61b82ac0600b82bd5c5
cagataycali <calicagatay@icloud.com>
2016-11-30 18:09:30 -0800

    ✨  Simple is the best, but simplicity is not easy

---
+++ README.md
@@ -0,0 +1,21 @@
+<h3 align="center">
+  <img src="logo.png" alt="l00g.in logo, ssh moderate cli tool." />
+</h3>
+<h4 align="center">
+  l00g.in, ssh moderate cli tool.
+</h4>
+---------------
+[![Version npm](https://img.shields.io/npm/v/l00g.in.svg?style=flat-square)](https://www.npmjs.com/package/l00g.in)[![npm Downloads](https://img.shields.io/npm/dm/l00g.in.svg?style=flat-square)](https://www.npmjs.com/package/l00g.in)[![Dependencies](https://img.shields.io/david/cagataycali/l00g.in.svg?style=flat-square)](https://david-dm.org/cagataycali/l00g.in.svg)
+[![GitHub stars](https://img.shields.io/github/stars/cagataycali/l00g.in.svg?style=social&label=Star&maxAge=2592000)]()
+
+# Install
+
+```shell
+npm i -g l00g.in
+```
+
+# Usage
+
+```
+l00g
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+var inquirer = require('inquirer');
+var colors = require('colors');
+var parser = require('./lib/parser');
+var add = require('./lib/add');
+var remove = require('./lib/remove');
+var sync = require('./lib/sync');
+
+var questions = [
+  {
+  	type: 'list',
+  	name: 'option',
+  	message: 'How can I help you,',
+  	choices: ['List', 'Add', 'Remove', 'Github sync'],
+  }
+];
+
+inquirer.prompt(questions).then(function (answers) {
+
+     // Maybe switch ?
+     if (answers.option === 'List') {
+       parser.stringify()
+        .then((configFile) => {
+          console.log(colors.gray(configFile));
+        })
+     } else if (answers.option === 'Add') {
+       add()
+         .then((output) => {
+           console.log(output);
+         })
+         .catch((err) => {
+           console.log(err);
+         })
+     } else if (answers.option === 'Remove') {
+       remove()
+        .then((value) => {
+          console.log(value);
+        })
+     } else if (answers.option === 'Github sync') {
+       sync()
+        .then((value) => {
+          console.log(value);
+        })
+     }
+
+});
