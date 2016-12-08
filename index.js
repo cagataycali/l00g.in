60cdb845aa69581dde9754fa12eaac7d02f042ea
cagataycali <calicagatay@icloud.com>
2016-12-08 15:49:51 -0800

    🐛  Bug fix2

--- index.js
+++ index.js
@@ -3,56 +3,72 @@ var async = require('async');
 var globalModulesDir = require('global-modules');
 var E = require('3x3c');
 
-console.log(globalModulesDir);
-
 E(`${globalModulesDir}/l00g.in/run.sh`)
   .then(() => {
 
     var inquirer = require('inquirer');
     var colors = require('colors');
-    var parser = require('./lib/parser');
-    var add = require('./lib/add');
-    var remove = require('./lib/remove');
-    var sync = require('./lib/sync');
-    
-    var questions = [
-      {
-      	type: 'list',
-      	name: 'option',
-      	message: 'How can I help you,',
-      	choices: ['List', 'Add', 'Remove', 'Github sync'],
-      }
-    ];
+    var fs = require('fs');
+    var configDir = process.env['HOME'] + '/.ssh/config';
+    var config = fs.readFileSync(configDir, 'utf8');
 
-    inquirer.prompt(questions).then(function (answers) {
+    if (config.trim().length === 0 ) {
+      var add = require('./lib/add');
+      console.log(colors.green('Your config is empty, let set your\'s first!'));
+      add()
+        .then((output) => {
+          console.log(output);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    } else {
 
-         // Maybe switch ?
-         if (answers.option === 'List') {
-           parser.stringify()
-            .then((configFile) => {
-              console.log(colors.gray(configFile));
-            })
-         } else if (answers.option === 'Add') {
-           add()
-             .then((output) => {
-               console.log(output);
-             })
-             .catch((err) => {
-               console.log(err);
-             })
-         } else if (answers.option === 'Remove') {
-           remove()
-            .then((value) => {
-              console.log(value);
-            })
-         } else if (answers.option === 'Github sync') {
-           sync()
-            .then((value) => {
-              console.log(value);
-            })
-         }
+      var parser = require('./lib/parser');
+      var add = require('./lib/add');
+      var remove = require('./lib/remove');
+      var sync = require('./lib/sync');
 
-    });
+      var questions = [
+        {
+        	type: 'list',
+        	name: 'option',
+        	message: 'How can I help you,',
+        	choices: ['List', 'Add', 'Remove', 'Github sync'],
+        }
+      ];
+
+      inquirer.prompt(questions).then(function (answers) {
+
+           // Maybe switch ?
+           if (answers.option === 'List') {
+             parser.stringify()
+              .then((configFile) => {
+                console.log(colors.gray(configFile));
+              })
+           } else if (answers.option === 'Add') {
+             add()
+               .then((output) => {
+                 console.log(output);
+               })
+               .catch((err) => {
+                 console.log(err);
+               })
+           } else if (answers.option === 'Remove') {
+             remove()
+              .then((value) => {
+                console.log(value);
+              })
+           } else if (answers.option === 'Github sync') {
+             sync()
+              .then((value) => {
+                console.log(value);
+              })
+           }
+
+      });
+
+    }
 
   })
   .catch((err) => {console.log(err);})
